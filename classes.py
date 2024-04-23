7d25f3f3a8055c33d505828d7f2d78be262ef067
djdonpablo <mael.reynaud@epita.fr>
2024-04-23 15:53:42 +0000

    [embedding] feat: some comments

--- classes.py
+++ classes.py
@@ -5,6 +5,7 @@ import numpy as np
 nltk.download('punkt')
 
 
+# naive bayesian, n-grams, logistic regression, NN
 class Benchmark:
     def __init__(self):
         self.pre_trained_embedding_models = ["fasttext-wiki-news-subwords-300", "word2vec-google-news-300", "glove-wiki-gigaword-300"]
@@ -26,3 +27,6 @@ class PreTrainedEmbedding:
 
 
 # embedder = PreTrainedEmbedding(pre_trained_embedding_models[0])
+
+# benchmark avec les modèles au dessus -> prendre le meilleur et faire un grid search, fine tuning
+# générer de la donnée 
